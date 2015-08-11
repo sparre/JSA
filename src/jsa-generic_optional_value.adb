19bea6a0101c2e3bed559cfae3ed9928d9138e60
Jacob Sparre Andersen <jacob@jacob-sparre.dk>
2015-08-11 20:32:36 +0200

    Importing package Generic_Something_Or_Nothing as JSA.Generic_Optional_Value.

---
+++ src/jsa-generic_optional_value.adb
@@ -0,0 +1,53 @@
+with Ada.Text_IO;
+
+package body JSA.Generic_Optional_Value is
+
+   function "+" (Item : in     Element) return Instance is
+   begin
+      return (Set   => True,
+              Value => Item);
+   end "+";
+
+   function "+" (Item : in     Instance) return Element is
+   begin
+      return Item.Value;
+   end "+";
+
+   function "=" (Left  : in     Element;
+                 Right : in     Instance) return Boolean is
+   begin
+      return Right.Set and then Left = Right.Value;
+   end "=";
+
+   function "=" (Left  : in     Instance;
+                 Right : in     Element) return Boolean is
+   begin
+      return Left.Set and then Left.Value = Right;
+   end "=";
+
+   function Image (Item : in     Instance) return String is
+   begin
+      case Item.Set is
+         when True =>
+            return Image (Item.Value);
+         when False =>
+            return "";
+      end case;
+   end Image;
+
+   function Value (Item : in     String) return Instance is
+   begin
+      if Item = "" then
+         return (Set   => False);
+      else
+         return (Set   => True,
+                 Value => Value (Item));
+      end if;
+   exception
+      when others =>
+         Ada.Text_IO.Put
+           (File => Ada.Text_IO.Current_Error,
+            Item => "JSA.Generic_Optional_Value.Value (""" & Item & """);");
+         raise;
+   end Value;
+end JSA.Generic_Optional_Value;
---
+++ src/jsa-generic_optional_value.ads
@@ -0,0 +1,30 @@
+generic
+   type Element is private;
+package JSA.Generic_Optional_Value is
+   type Instance (Set : Boolean := False) is
+      record
+         case Set is
+            when True =>
+               Value : Element;
+            when False =>
+               null;
+         end case;
+      end record;
+
+   function "+" (Item : in     Element) return Instance;
+   function "+" (Item : in     Instance) return Element
+     with Pre => Item.Set;
+
+   function "=" (Left  : in     Element;
+                 Right : in     Instance) return Boolean;
+   function "=" (Left  : in     Instance;
+                 Right : in     Element) return Boolean;
+
+   generic
+      with function Image (Item : in     Element) return String;
+   function Image (Item : in     Instance) return String;
+
+   generic
+      with function Value (Item : in     String) return Element;
+   function Value (Item : in     String) return Instance;
+end JSA.Generic_Optional_Value;
